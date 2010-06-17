9fbadd293ba522ee082bcd576b907efd962d50fb
John Le <john@doloreslabs.com>
2010-06-17 02:23:24 -0700

    word counter example

--- lib/classes/word_counter.rb
+++ lib/classes/word_counter.rb
@@ -1,6 +1,7 @@
 class WordCounter < MapRedus::Mapper
   def self.map(map_data)
-    map_data.split.map do |word|
+    map_data.split(/\W/).each do |word|
+      next if word.empty?
       yield(word.downcase, 1)
     end
   end
