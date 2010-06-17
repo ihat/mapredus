28c84d74d81e51e312673fb41cc8d569a40f53a0
John Le <john@doloreslabs.com>
2010-06-17 02:22:54 -0700

    testing stuff

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,103 @@
 require 'rubygems'
 require 'test/unit'
-require 'shoulda'
 
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift(File.join(dir, '..', 'lib'))
+$LOAD_PATH.unshift(dir)
 require 'mapreduce'
 
-class Test::Unit::TestCase
+##
+# test/spec/mini 3
+# http://gist.github.com/25455
+# chris@ozmm.org
+#
+def context(*args, &block)
+  return super unless (name = args.first) && block
+  require 'test/unit'
+  klass = Class.new(defined?(ActiveSupport::TestCase) ? ActiveSupport::TestCase : Test::Unit::TestCase) do
+    def self.test(name, &block)
+      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
+    end
+    def self.xtest(*args) end
+    def self.setup(&block) define_method(:setup, &block) end
+    def self.teardown(&block) define_method(:teardown, &block) end
+  end
+  (class << klass; self end).send(:define_method, :name) { name.gsub(/\W/,'_') }
+  klass.class_eval &block
+end
+
+#
+# make sure we can run redis
+#
+if !system("which redis-server")
+  puts '', "** can't find `redis-server` in your path"
+  puts "** try running `sudo rake install`"
+  abort ''
+end
+
+#
+# start our own redis when the tests start,
+# kill it when they end
+#
+at_exit do
+  next if $!
+
+  if defined?(MiniTest)
+    exit_code = MiniTest::Unit.new.run(ARGV)
+  else
+    exit_code = Test::Unit::AutoRunner.run
+  end
+
+  pid = `ps -A -o pid,command | grep [r]edis-test`.split(" ")[0]
+  puts "Killing test redis server..."
+  `rm -f #{dir}/dump.rdb`
+  Process.kill("KILL", pid.to_i)
+  exit exit_code
+end
+
+puts "Starting redis for testing at localhost:9736..."
+`redis-server #{dir}/redis-test.conf`
+
+MapRedus.redis = 'localhost:9736:0'
+Resque.redis = MapRedus.redis
+
+class GetWordCount < MapRedus::Job
+  def self.specification(data)
+    {
+      :mapper => WordCounter,
+      :reducer => Adder,
+      :finalizer => ToHash,
+      :data => data,
+      :ordered => false,
+      :extra_data => {}
+    }
+  end
+end
+
+class WordCounter < MapRedus::Mapper
+  def self.partition_size; 1; end
+  def self.map(map_data)
+    map_data.split(/\W/).each do |word|
+      next if word.empty?
+      yield(word.downcase, 1)
+    end
+  end
+end
+
+class Adder < MapRedus::Reducer
+  def self.reduce(value_list)
+    yield( value_list.reduce(0) { |r, v| r += v.to_i } )
+  end
+end
+
+class ToHash < MapRedus::Finalizer
+  def self.finalize
+    result = {}
+    each_key_value(pid) do |key, value|
+      result[key] = value.to_i
+    end
+    MapRedus::Job.save_result(MapReduce::Support.encode(result), pid, "test:result")
+    MapRedus::Job.cleanup(pid)
+    result
+  end
 end
--- test/test_mapreduce.rb
+++ test/test_mapreduce.rb
@@ -1,7 +1,19 @@
-require 'helper'
+require File.dirname(__FILE__) + '/helper'
 
-class TestMapreduce < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+context "MapRedus" do
+  setup do
+    @pid = GetWordCount.create(["There was nothing so VERY remarkable in that; nor did Alice think it so
+VERY much out of the", "way to hear the Rabbit say to itself, 'Oh dear!
+Oh dear! I shall be late!'", "(when she thought it over afterwards, it
+occurred to her that she ought", "to have wondered at this, but at the time
+it all seemed quite natural);"])
+
+    @word_count = {"natural"=>1, "over"=>1, "say"=>1, "it"=>4, "think"=>1, "all"=>1, "but"=>1, "of"=>1, "quite"=>1, "much"=>1, "alice"=>1, "very"=>2, "be"=>1, "shall"=>1, "oh"=>2, "hear"=>1, "out"=>1, "time"=>1, "way"=>1, "nothing"=>1, "seemed"=>1, "occurred"=>1, "she"=>2, "itself"=>1, "to"=>4, "in"=>1, "wondered"=>1, "ought"=>1, "rabbit"=>1, "the"=>3, "nor"=>1, "so"=>2, "thought"=>1, "at"=>2, "have"=>1, "when"=>1, "remarkable"=>1, "there"=>1, "afterwards"=>1, "i"=>1, "dear"=>2, "did"=>1, "that"=>2, "was"=>1, "this"=>1, "her"=>1, "late"=>1}
+  end
+
+  test "test running a map reduce job synchronously" do
+    GetWordCount.run(@pid, synchronously = true)
+    mr_result = MapRedus::Support.decode(GetWordCount.get_saved_result("test:result"))
+    assert_equal @word_count, mr_result
   end
 end
