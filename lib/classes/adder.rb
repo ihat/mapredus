aff6e3a467050b6db055add107ccfc90849710ad
John Le <john@doloreslabs.com>
2010-06-18 03:07:47 -0700

    removing files that are not being used

--- lib/classes/adder.rb
+++
@@ -1,5 +0,0 @@
-class Adder < MapRedus::Reducer
-  def self.reduce(value_list)
-    yield( value_list.reduce(0) { |r, v| r += v.to_i } )
-  end
-end
--- lib/classes/word_counter.rb
+++
@@ -1,8 +0,0 @@
-class WordCounter < MapRedus::Mapper
-  def self.map(map_data)
-    map_data.split(/\W/).each do |word|
-      next if word.empty?
-      yield(word.downcase, 1)
-    end
-  end
-end
