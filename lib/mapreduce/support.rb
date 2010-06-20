e848d896ec82871a0bf47739e61717367f669c69
John Le <john@doloreslabs.com>
2010-06-20 16:25:03 -0700

    support name_spaced so that different classes can define mapreduce process with the same name

--- lib/mapreduce/support.rb
+++ lib/mapreduce/support.rb
@@ -3,22 +3,42 @@ module MapRedus
     class MapRedusRunnerError < StandardError; end
     class DuplicateProcessDefinitionError < MapRedusRunnerError ; end
 
-    module Runner; end
-    def mapreduce; Runner; end
+    class Runner
+      def initialize(class_name)
+        @class = class_name
+      end
+      
+      def method_missing(method, *args, &block)
+        mr_process = "#{@class}_#{method.to_s}"
+        if self.class.respond_to?(mr_process)
+          self.class.send(mr_process, *args, &block)
+        else
+          super(method, *args, &block)
+        end
+      end
+    end
+
+    def mapreduce
+      @@mapreduce_runner ||= Runner.new(self.class.to_s.gsub(/\W/,"_"))
+    end
     
     module ClassMethods
       def mapreduce_process( process_name, mapper, reducer, finalizer, outputter, result_store, opts = {})
-        runner_self = (class << Support::Runner; self; end)
-        raise DuplicateProcessDefintionError if runner_self.methods.include? process_name.to_s
+        runner_self = (class << Runner; self; end)
+        class_name = self.to_s.gsub(/\W/,"_")
+        
+        global_process_name = "#{class_name}_#{process_name.to_s}"
+        
+        if runner_self.methods.include?(global_process_name)
+          raise DuplicateProcessDefintionError
+        end
 
-        runner_self = (class << Support::Runner; self; end)
-
-        runner_self.send( :define_method, process_name.to_s ) do |data|
+        runner_self.send( :define_method, global_process_name ) do |data|
           process = MapRedus::Process.create(mapper, reducer, finalizer, outputter, result_store, data)
           process.run
         end
 
-        runner_self.send( :define_method, "#{process_name.to_s}_result" ) do
+        runner_self.send( :define_method, "#{global_process_name}_result" ) do
           outputter.decode(MapRedus::Process.get_saved_result(result_store))
         end
       end
